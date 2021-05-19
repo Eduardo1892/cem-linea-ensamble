65541b6f320ef4b97c562f6ef9e198257b622493
eduardo alvarez <ed.alvarezv96@gmail.com>
2021-05-19 10:53:56 -0400

    Se agregan campos a la consulta del dashboard para listar las estaciones y sus detalles.

--- controllers/dashBoardController.js
+++ controllers/dashBoardController.js
@@ -8,7 +8,35 @@ const listarEstacionesDashBoard = async (req, res) => {
             'codigo', 
             'descripcion',
             [Sequelize.literal(`(SELECT COUNT(DISTINCT codigo) FROM paquetes 
-            WHERE codigo_estacion = estacion.codigo AND fecha_sys = CURDATE())`),'paquetes']
+            WHERE codigo_estacion = estacion.codigo AND fecha_sys = CURDATE())`),'paquetes'],
+            [Sequelize.literal(`(SELECT it.descripcion 
+            FROM paquetes pq
+            LEFT JOIN items it ON it.codigo = pq.codigo_item
+            WHERE codigo_estacion = estacion.codigo
+            ORDER BY fecha_sys DESC, hora_sys DESC
+            LIMIT 1)`), 'codigo_ultimo_item'],
+            [Sequelize.literal(`(SELECT est.descripcion 
+            FROM paquetes pq
+            LEFT JOIN estaciones est ON est.codigo = pq.codigo_estacion
+            WHERE codigo_estacion = estacion.codigo
+            ORDER BY fecha_sys DESC, hora_sys DESC
+            LIMIT 1)`), 'codigo_ultima_estacion'],
+            [Sequelize.literal(`(SELECT fecha_sys
+            FROM paquetes
+            WHERE codigo_estacion = estacion.codigo
+            ORDER BY fecha_sys DESC, hora_sys DESC
+            LIMIT 1)`), 'ultima_fecha'],
+            [Sequelize.literal(`(SELECT hora_sys
+            FROM paquetes
+            WHERE codigo_estacion = estacion.codigo
+            ORDER BY fecha_sys DESC, hora_sys DESC
+            LIMIT 1)`), 'ultima_hora'],
+            [Sequelize.literal(`(SELECT us.nombre 
+            FROM paquetes pq
+            LEFT JOIN usuarios us ON us.codigo = pq.codigo_usuario
+            WHERE codigo_estacion = estacion.codigo
+            ORDER BY fecha_sys DESC, hora_sys DESC
+            LIMIT 1)`), 'codigo_ultimo_usuario']
         ],
         order: [
             ['descripcion', 'ASC'],
@@ -18,6 +46,7 @@ const listarEstacionesDashBoard = async (req, res) => {
     res.json({
         estaciones
     })
+
 
 }
 
